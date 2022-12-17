fc6bde97cc69123f3c9643716eaa6430cb7fbaf7
Mahmoud Basha <mbasga14@hotmail.com>
2022-12-17 20:07:57 +0300

    DbContext added

--- E-Commerce/Program.cs
+++ E-Commerce/Program.cs
@@ -1,9 +1,17 @@
+using E_Commerce.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
